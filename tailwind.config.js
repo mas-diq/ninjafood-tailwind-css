6635bb20f68a8595657dec72f0d2c3e553472c43
Muhammad Shiddiq Fathullah <msf31@student.gunadarma.ac.id>
2021-02-10 14:31:42 +0700

    make some custom styles in tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        primary: '#ff6363',
+        secondary:{
+          100: '#ff0360',
+          200: '#bb6363'
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
